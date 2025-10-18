1394ea389c883fb11ef24040a09d126e173d6d4a
Alagar <alagar17302@gmail.com>
2025-10-18 22:41:03 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import app from './app.js';
-import { PORT } from './config/index.js';
+import app from './src/app.js';
+import { PORT } from './src/config/index.js';
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
